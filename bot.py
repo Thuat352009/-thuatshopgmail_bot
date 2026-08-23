f7016cb8297a21ad1f69b2df91480ac3ca0914ed
Thuat352009 <thuatticktock@gmail.com>
2026-08-23 16:28:31 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,14 +5,10 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("👋 Chào mừng! Bot đã hoạt động")
+    await update.message.reply_text("Xin chào! Bot đã hoạt động 24/24 trên Render 💪")
 
-async def sanpham(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("📦 Danh sách sản phẩm")
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
 
 if __name__ == "__main__":
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("sanpham", sanpham))
-    print("Bot đang chạy...")
     app.run_polling()
