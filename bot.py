660202a853dc8dc0a0fb52ee884d7fa8a7ed23f4
Thuat352009 <thuatticktock@gmail.com>
2026-08-23 16:39:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import os
+import asyncio
+import nest_asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+nest_asyncio.apply() # Dòng này fix lỗi Render
 
 TOKEN = os.getenv("TOKEN")
 
@@ -11,4 +15,6 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 if __name__ == "__main__":
-    app.run_polling()
+    # Cách chạy cho Render
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(app.run_polling())
